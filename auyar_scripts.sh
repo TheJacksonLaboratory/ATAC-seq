eccd874d534816668d668d03bdc9ea740541c1c2
Shane Sanders <shane.sanders@jax.org>
2016-06-25 07:24:55 -0400

    Changed trimmomatic command to unify with auyar bwa_trimmomatic.sh (and hopefully the following scripts).  I suspect something is still broken there though.  Hopefully fixed the job array dependency.

--- auyar_scripts.sh
+++ auyar_scripts.sh
@@ -38,10 +38,10 @@ echo \#PBS -t 1-$FILENUMBER >> $outputDIR/trimmomatic.qsub
 echo module load python >> $outputDIR/trimmomatic.qsub
 echo FILE=\$\(head -n \$PBS_ARRAYID $outputDIR/trimmomatic_R1_filelist.txt \| tail -1\) >> $outputDIR/trimmomatic.qsub
 echo FILE2=\$\(basename "\${FILE}" \| sed \'s/R1_001\.fastq.gz/R2_001\.fastq.gz/g\'\) >> $outputDIR/trimmomatic.qsub
-echo FILE1paired=\$\(basename \"\${FILE}\" \| sed \'s/R1_001\.fastq.gz/R1_001\.fastq_filtered.gz/g\'\) >> $outputDIR/trimmomatic.qsub
-echo FILE2paired=\$\(basename \"\${FILE2}\" \| sed \'s/R2_001\.fastq.gz/R2_001\.fastq_filtered.gz/g\'\) >> $outputDIR/trimmomatic.qsub
-echo FILE1unpaired=\$\(basename \"\${FILE}\" \| sed \'s/R1_001\.fastq.gz/R1_001\.trimU\.fastq.gz/g\'\) >> $outputDIR/trimmomatic.qsub
-echo FILE2unpaired=\$\(basename \"\${FILE2}\" \| sed \'s/R2_001\.fastq.gz/R2_001\.trimU\.fastq.gz/g\'\) >> $outputDIR/trimmomatic.qsub
+echo FILE1paired=\$\(basename \"\${FILE}\" \| sed \'s/R1_001\.fastq.gz/R1_001\.trim\.fastq\.gz/g\'\) >> $outputDIR/trimmomatic.qsub
+echo FILE2paired=\$\(basename \"\${FILE2}\" \| sed \'s/R2_001\.fastq.gz/R2_001\.trim\.fastq\.gz/g\'\) >> $outputDIR/trimmomatic.qsub
+echo FILE1unpaired=\$\(basename \"\${FILE}\" \| sed \'s/R1_001\.fastq.gz/R1_001\.trimU\.fastq\.gz/g\'\) >> $outputDIR/trimmomatic.qsub
+echo FILE2unpaired=\$\(basename \"\${FILE2}\" \| sed \'s/R2_001\.fastq.gz/R2_001\.trimU\.fastq\.gz/g\'\) >> $outputDIR/trimmomatic.qsub
 echo java -jar /opt/compsci/Trimmomatic/0.33/trimmomatic-0.33.jar PE -threads 2 \$FILE $dataDIR\$FILE2 $outputDIR/trimmomatic/\$FILE1paired $outputDIR/trimmomatic/\$FILE1unpaired $outputDIR/trimmomatic/\$FILE2paired $outputDIR/trimmomatic/\$FILE2unpaired TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36 >> $outputDIR/trimmomatic.qsub
 echo python $scriptDIR/auyar/pyadapter_trim.py -a $outputDIR/trimmomatic/\$FILE1paired -b $outputDIR/trimmomatic/\$FILE2paired >> $outputDIR/trimmomatic.qsub
 
@@ -66,38 +66,42 @@ mkdir macs2
 mkdir homer
 cd ../..
 currentDIR=$(pwd)
-#echo $currentDIR
+#echo "Current DIR" $currentDIR
+#echo "Output DIR" $outputDIR
 
-echo \#!/bin/bash >> $outputDIR/ATAC-seq.qsub
-echo \#PBS -l nodes=1:ppn=16 >> $outputDIR/ATAC-seq.qsub
-echo \#PBS -l walltime=16:00:00 >> $outputDIR/ATAC-seq.qsub
-echo \#PBS -N ATAC-seq  >> $outputDIR/ATAC-seq.qsub
-echo module load python >> $outputDIR/ATAC-seq.qsub
-echo module load R >> $outputDIR/ATAC-seq.qsub
-echo module load perl/5.10.1 >> $outputDIR/ATAC-seq.qsub
-echo module load samtools/0.1.19 >> $outputDIR/ATAC-seq.qsub
-echo module load bedtools >> $outputDIR/ATAC-seq.qsub
-echo gunzip $outputDIR/trimmomatic/*.gz >> $outputDIR/ATAC-seq.qsub
+cp $scriptDIR/auyar/run_ATAC_pipeline.sh $currentDIR/
+
+#echo \#!/bin/bash >> $outputDIR/ATAC-seq.qsub
+#echo \#PBS -l nodes=1:ppn=16 >> $outputDIR/ATAC-seq.qsub
+#echo \#PBS -l walltime=16:00:00 >> $outputDIR/ATAC-seq.qsub
+#echo \#PBS -N ATAC-seq  >> $outputDIR/ATAC-seq.qsub
+#echo module load python >> $outputDIR/ATAC-seq.qsub
+#echo module load R >> $outputDIR/ATAC-seq.qsub
+#echo module load perl/5.10.1 >> $outputDIR/ATAC-seq.qsub
+#echo module load samtools/0.1.19 >> $outputDIR/ATAC-seq.qsub
+#echo module load bedtools >> $outputDIR/ATAC-seq.qsub
+#echo gunzip $outputDIR/trimmomatic/*.gz >> $outputDIR/ATAC-seq.qsub
 #echo bash $scriptDIR/auyar/bwa_trimmomatic.sh ../ >> $outputDIR/ATAC-seq.qsub
 
-ls $outputDIR/trimmomatic
-for f in $(find $outputDIR/trimmomatic/ -name *R1_001.trim.fastq.gz)
-do
-	fileName2=$(basename "${f}" | sed 's/R1_001\.trim\.fastq/R2_001\.trim\.fastq/g')
-	fileNameSAM=$(basename "${f}" | sed 's/R1_001\.trim\.fastq/hg19\.sam/g')
-	fileNameMetrics=$(basename "${f}" | sed 's/R1_001\.trim\.fastq/metrics\.txt/g')
-	echo /opt/compsci/bwa/0.7.12/bin/bwa mem -M /data/shared/genomes/Homo_sapiens/UCSC/hg19/Sequence/BWAIndex/genome.fa $f $outputDIR/trimmomatic/$fileName2 > $outputDIR/trimmomatic/bwa/$fileNameSAM
-done
+#ls $outputDIR/trimmomatic
+#for f in $(find $outputDIR/trimmomatic/ -name *R1_001.trim.fastq.gz)
+#do
+	#fileName2=$(basename "${f}" | sed 's/R1_001\.trim\.fastq/R2_001\.trim\.fastq/g')
+	#fileNameSAM=$(basename "${f}" | sed 's/R1_001\.trim\.fastq/hg19\.sam/g')
+	#fileNameMetrics=$(basename "${f}" | sed 's/R1_001\.trim\.fastq/metrics\.txt/g')
+	#echo /opt/compsci/bwa/0.7.12/bin/bwa mem -M /data/shared/genomes/Homo_sapiens/UCSC/hg19/Sequence/BWAIndex/genome.fa $f $outputDIR/trimmomatic/$fileName2 > $outputDIR/trimmomatic/bwa/$fileNameSAM
+#done
 
 
-echo bash $scriptDIR/auyar/macs2_bwa_trimmomatic.sh ../ >> $outputDIR/ATAC-seq.qsub
-echo bash $scriptDIR/auyar/homer_bwa_trimmomatic.sh ../ >> $outputDIR/ATAC-seq.qsub
-echo bash $scriptDIR/auyar/nReads.sh ../ >> $outputDIR/ATAC-seq.qsub
-echo gzip $outputDIR/trimmomatic/*.fastq >> $outputDIR/ATAC-seq.qsub
+#echo bash $scriptDIR/auyar/macs2_bwa_trimmomatic.sh ../ >> $outputDIR/ATAC-seq.qsub
+#echo bash $scriptDIR/auyar/homer_bwa_trimmomatic.sh ../ >> $outputDIR/ATAC-seq.qsub
+#echo bash $scriptDIR/auyar/nReads.sh ../ >> $outputDIR/ATAC-seq.qsub
+#echo gzip $outputDIR/trimmomatic/*.fastq >> $outputDIR/ATAC-seq.qsub
 ######
 
 # something buggy is going on here...do job dependencies bug out when given job arrays?
 # commenting the automatic queue submission section out for debugging at this point
-# qsub $outputDIR/fastqc.qsub
-# JOBHOLD="$(qsub $outputDIR/trimmomatic.qsub)"
-# qsub -Wdepend=afterok:$JOBHOLD $outputDIR/ATAC-seq.qsub
+
+qsub $outputDIR/fastqc.qsub
+JOBHOLD="$(qsub $outputDIR/trimmomatic.qsub)"
+qsub -Wdepend=afterokarray:$JOBHOLD $outputDIR/run_ATAC_pipeline.sh
