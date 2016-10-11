95466864b48c3c8c6d70487b0eb9d71d43f7d8d8
Anne Deslattes Mays <adeslat@jax.org>
2016-10-11 10:25:36 -0400

    updated to run correctly

--- bwa_postpostalignment.sh
+++ bwa_postpostalignment.sh
@@ -1,34 +1,35 @@
 #!/bin/bash
 
-dataDIR=$1 #ARGV, contains folder with FASTQ files, right now needs full path
+#dataDIR=$1 #ARGV, contains folder with FASTQ files, right now needs full path
 
 scriptDIR=$(pwd)
-inputDIR=$dataDIR/trimmomatic/bwa
-outputDIR=$dataDIR/trimmomatic/bwa
+workingDIR=$scriptDIR/working
+inputDIR=$workingDIR/trimmomatic/bwa
+outputDIR=$workingDIR/trimmomatic/bwa
 #
 # now bamtobed
 #
-rm $dataDIR/shiftedbamfilelist.txt
-rm $dataDIR/postPost.qsub
+rm $workingDIR/shiftedbamfilelist.txt
+rm $workingDIR/postPostBWA.qsub
 
-ls -1 $inputDIR/*shifted_sorted.bam > $dataDIR/shiftedbamfilelist.txt
-FILENUMBER=$(wc -l $dataDIR/shiftedbamfilelist.txt | cut -d' ' -f1)
+ls -1 $inputDIR/*shifted.bam > $workingDIR/shiftedbamfilelist.txt
+FILENUMBER=$(wc -l $workingDIR/shiftedbamfilelist.txt | cut -d' ' -f1)
 
-echo \#!/bin/bash > $dataDIR/postPostBWA.qsub
-echo \#PBS -l nodes=1:ppn=16 >> $dataDIR/postPostBWA.qsub
-echo \#PBS -l walltime=12:00:00 >> $dataDIR/postPostBWA.qsub
-echo \#PBS -N bwa  >> $dataDIR/postPostBWA.qsub
-echo \#PBS -t 1-$FILENUMBER >> $dataDIR/postPostBWA.qsub
-echo module load python >> $dataDIR/postPostBWA.qsub
-echo module load R >> $dataDIR/postPostBWA.qsub
-echo module load perl/5.10.1 >> $dataDIR/postPostBWA.qsub
-echo module load samtools/0.1.19 >> $dataDIR/postPostBWA.qsub
-echo module load bedtools >> $dataDIR/postPostBWA.qsub
-echo FILE=\$\(head -n \$PBS_ARRAYID $dataDIR/shiftedbamfilelist.txt \| tail -1\) >> $dataDIR/postPostBWA.qsub
-echo FILENAME=\$\(basename \"\${FILE}\" \| sed \'s/\.bam/_sorted.bam/g\'\)  >> $dataDIR/postPostBWA.qsub
-echo FILENAME1=\$\(basename \"\${FILE}\" \| sed \'s/\.bam/_sorted/g\'\)  >> $dataDIR/postPostBWA.qsub
-echo FILENAME2=\$\(basename \"\${FILE}\" \| sed \'s/\.bam/_sorted.bed/g\'\) >> $dataDIR/postPostBWA.qsub
-echo samtools sort \$FILE $outputDIR/\$FILENAME1 >> $dataDIR/postPostBWA.qsub
-echo samtools index $outputDIR/\$FILENAME >> $dataDIR/postPostBWA.qsub
-echo bedtools bamtobed -i $outputDIR/\$FILENAME \> $outputDIR/\$FILENAME2 >> $dataDIR/postPostBWA.qsub
+echo \#!/bin/bash > $workingDIR/postPostBWA.qsub
+echo \#PBS -l nodes=1:ppn=16 >> $workingDIR/postPostBWA.qsub
+echo \#PBS -l walltime=12:00:00 >> $workingDIR/postPostBWA.qsub
+echo \#PBS -N bwa  >> $workingDIR/postPostBWA.qsub
+echo \#PBS -t 1-$FILENUMBER >> $workingDIR/postPostBWA.qsub
+echo module load python >> $workingDIR/postPostBWA.qsub
+echo module load R >> $workingDIR/postPostBWA.qsub
+echo module load perl/5.10.1 >> $workingDIR/postPostBWA.qsub
+echo module load samtools/0.1.19 >> $workingDIR/postPostBWA.qsub
+echo module load bedtools >> $workingDIR/postPostBWA.qsub
+echo FILE=\$\(head -n \$PBS_ARRAYID $workingDIR/shiftedbamfilelist.txt \| tail -1\) >> $workingDIR/postPostBWA.qsub
+echo FILENAME=\$\(basename \"\${FILE}\" \| sed \'s/\.bam/_sorted.bam/g\'\)  >> $workingDIR/postPostBWA.qsub
+echo FILENAME1=\$\(basename \"\${FILE}\" \| sed \'s/\.bam/_sorted/g\'\)  >> $workingDIR/postPostBWA.qsub
+echo FILENAME2=\$\(basename \"\${FILE}\" \| sed \'s/\.bam/_sorted.bed/g\'\) >> $workingDIR/postPostBWA.qsub
+echo samtools sort \$FILE $outputDIR/\$FILENAME1 >> $workingDIR/postPostBWA.qsub
+echo samtools index $outputDIR/\$FILENAME >> $workingDIR/postPostBWA.qsub
+echo bedtools bamtobed -i $outputDIR/\$FILENAME \> $outputDIR/\$FILENAME2 >> $workingDIR/postPostBWA.qsub
 
